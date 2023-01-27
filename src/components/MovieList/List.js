f6ed9a50acf2702885fa9c00f16472635b367daa
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-26 17:28:21 -0700

    fixed axios

--- src/components/MovieList/List.js
+++ src/components/MovieList/List.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'; 
-import axios from './axios';
+import axios from "./axios"; 
 
 
 const base_url= "https://image.tmdb.org/t/p/w500" //baseURL link for picture of poster 
