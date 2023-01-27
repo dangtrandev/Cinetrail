9575dcb73bcb69b9538b6d35af2e91cab02b5e69
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-26 18:02:36 -0700

    using useEffect to fecth data from tmdb

--- src/List.js
+++ src/List.js
@@ -2,20 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from "./axios"; 
 
 const base_url= "https://image.tmdb.org/t/p/w500" //baseURL link for picture of poster 
-function List(fetchUrl, title) {
+
+function List({fetchUrl, title}) {
     const [movies, setMovies] = useState([]); //set the initial value to an empty array
 
     useEffect(() => {
       async function fetchData(){
-        const request = axios.get(fetchUrl); 
-        console.log(request); //checking the type of data which will be fetched 
+        const request = await axios.get(fetchUrl); 
+        // console.log(request); //checking the type of data which will be fetched 
+        setMovies(request.data.results);
+        return request; 
       }
       fetchData();
-    }, []) //the empty bracket allow run once only when the page refreshed
+    }, [fetchUrl]) //the empty bracket allow run once only when the page refreshed
   return (
-    <div className="list_container">
-        <div>
-            
+    <div className="playlist_container">
+        <h2>{title}</h2>
+        <div className="posters_container">
+            {movies.map(
+              movie => <img
+               key={movie?.id}
+               className="movie_poster"
+               title={movie?.name || movie?.orginal_title || movie?.title}
+               src={`${base_url}${movie?.poster_path}`}
+               alt={movie.name}/>
+            )}
         </div>
     </div>
   )
