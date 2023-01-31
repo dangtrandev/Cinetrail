7c39c15d33b613b1a54678fbbc1c3ee5b6ae107a
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-31 14:48:08 -0700

    checking the data

--- src/Banner.js
+++ src/Banner.js
@@ -7,27 +7,21 @@ const base_url= "https://image.tmdb.org/t/p/w500" //baseURL link for picture of 
 function Banner({fetchUrl}) {
 
   //using the useEffect to get the data from themoviedb.org
-  const [movie, setMovies] = useState([]); //set the initial value to an empty array
+  const [movies, setMovies] = useState([]); //set the initial value to an empty array
   
   useEffect(() => {
     async function fetchData() {
         const request = await axios.get(fetchUrl); 
-        // console.log(request); //checking the data get from the api 
+        console.log(request); //checking the data get from the api 
         setMovies(request.data.results); 
         return request; 
     }
     fetchData(); 
-  }, [fetchUrl])
+  }, [fetchUrl]);
 
   return (
     <div className='banner_container'>
-      <button></button>
-      <img 
-      className="movie_banner"
-      src={`${base_url}${movie[0]?.poster_path}`} 
-      alt={movie[0]?.title}/>
-      <button></button>
-
+     {movies && movies[0].original_title}
     </div>
   )
 }
