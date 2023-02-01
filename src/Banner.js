d4b33d926f3f9ac008fcf575b5b52c0d2d17950a
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-01 13:26:25 -0700

    fixing the handle left and handle right

--- src/Banner.js
+++ src/Banner.js
@@ -7,14 +7,12 @@ import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import StarRatings from "react-star-ratings";
 
 
-const base_url= "https://image.tmdb.org/t/p/w500" //baseURL link for picture of poster 
-
 function Banner({fetchUrl}) {
-
+  const base_url= "https://image.tmdb.org/t/p/w500" //baseURL link for picture of poster 
   //using the useEffect to get the data from themoviedb.org
   const [movies, setMovies] = useState([]); //set the initial value to an empty array
-  const [index, setIndex] = useState([0]);
-  useEffect(() => {
+  const [index, setIndex] = useState(0);
+  useEffect(() => { 
     async function fetchData() {
         const request = await axios.get(fetchUrl); 
         console.log(request); //checking the data get from the api 
@@ -24,13 +22,20 @@ function Banner({fetchUrl}) {
     fetchData(); 
   }, [fetchUrl]);
   // }, []);
+
+  // useEffect(()=>{
+  //   axios.get(fetchUrl)
+  //   .then((result) => {setMovies(result.data.results)})
+  //   .catch((err) => {console.log(err)})
+  // })
   //arrow function for clicking the left arrow 
   const handelLeft =()=>{
     if(index === 0) {
       setIndex(movies.length -1); 
       return; 
     } else {
-      setIndex((preIndex) =>(preIndex -1))
+      setIndex((prevIndex) => prevIndex -1)
+      
     }
   }
  
@@ -39,7 +44,7 @@ function Banner({fetchUrl}) {
     if(index === movies.length -1 ){
       setIndex(0);
     } else {
-      setIndex( (preIndex) => (preIndex + 1))
+      setIndex((prevIndex) => prevIndex + 1)
     }
   }
 
@@ -62,7 +67,7 @@ function Banner({fetchUrl}) {
           {/* <p className="banner_genre">{movies[index]?.genre_ids[index].name}</p> */}
           <p className="banner_release_date">{`Release Date: ${movies[index]?.release_date}`}</p>
           <StarRatings 
-            rating={movies[index]?.vote_average /2} //vote_average is on the scale of 10
+            rating={movies[index]?.vote_average / 2} //vote_average is on the scale of 10
             starRatedColor="red"
             starDimension="15px"
             starSpacing="1px"
