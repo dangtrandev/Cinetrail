b999de6ae1d06ab853c6facc6d1e574bf4535d0f
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-01-31 17:13:54 -0700

    importing the starratings to banner

--- src/Banner.js
+++ src/Banner.js
@@ -4,7 +4,7 @@ import axios from "./axios";
 import "./Banner.css"; 
 
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
-// import StarRatings from "react-star-ratings";
+import StarRatings from "react-star-ratings";
 
 
 const base_url= "https://image.tmdb.org/t/p/w500" //baseURL link for picture of poster 
@@ -22,7 +22,8 @@ function Banner({fetchUrl}) {
         return request; 
     }
     fetchData(); 
-  }, [fetchUrl]);
+  // }, [fetchUrl]);
+  }, []);
   //arrow function for clicking the left arrow 
   const handelLeft =()=>{
     if(index === 0) {
@@ -56,16 +57,22 @@ function Banner({fetchUrl}) {
           ></MdKeyboardArrowRight>
 
         <div className="banner_info">
-          <h1 className="banner_title">{movies[0].title}</h1>
-          <p className="banner_description">{movies[0].overview}</p>
-          <p className="banner_genre">{movies[0].genre_ids[0].name}</p>
-          <p className="banner_release_date">{`Release Date: ${movies[0].release_date}`}</p>
+          <h1 className="banner_title">{movies[0]?.title}</h1>
+          <p className="banner_description">{movies[0]?.overview}</p>
+          <p className="banner_genre">{movies[0]?.genre_ids[0].name}</p>
+          <p className="banner_release_date">{`Release Date: ${movies[0]?.release_date}`}</p>
+          <StarRatings 
+            ratings={movies[0]?.vote_average / 2}
+            starRatedColor="red"
+            starDemension="15px"
+            starSpacing="1px"
+          />
         </div>
      
       </div>
       <img 
       className='banner_poster'
-      src={`${base_url}${movies[0].backdrop_path}`}/>
+      src={`${base_url}${movies[0]?.backdrop_path}`}/>
     </div>
   )
 }
